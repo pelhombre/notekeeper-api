20a633dccdf09f24e6625a9ee98edce3ee919dcd
tictools <jaumevillarreal@gmail.com>
2024-04-28 22:41:48 +0200

    fix: sort exported methods

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,7 +1,8 @@
-const log = globalThis.console.log;
-
-const lineFeed = () => globalThis.console.log("\n");
 const lineDivider = () =>
   globalThis.console.log("+++++++++++++++++++++++++++++++++++++++++++++++++");
+
+const lineFeed = () => globalThis.console.log("\n");
+
+const log = globalThis.console.log;
 
 export default { lineDivider, lineFeed, log };
